1d6511426161257180b8113a250af90283ef9007
LutWrld <dibrahimalang@gmail.com>
2025-07-03 11:58:59 +0000

    delete unused splash screen method

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,21 +3,9 @@ import 'package:flutter/material.dart';
 
 import 'favorite_posts.dart';
 import 'posts.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
-
-  void initialization() async {
-    debugPrint('ready in 3...');
-    await Future.delayed(const Duration(seconds: 1));
-    debugPrint('ready in 2...');
-    await Future.delayed(const Duration(seconds: 1));
-    debugPrint('ready in 1...');
-    await Future.delayed(const Duration(seconds: 1));
-    debugPrint('go!');
-    FlutterNativeSplash.remove();
-  }
 
   @override
   Widget build(BuildContext context) {
